28cffa2c0dea3e22b969d4e5a8d516c4a283a81b
ashley3458 <19938768@clases.edu.sv>
2026-06-12 15:32:14 -0600

    Create Categoria.java

---
+++ Categoria.java
@@ -0,0 +1,20 @@
+public class Categoria {
+    private int idCategoria;
+    private String nombre;
+
+    public Categoria(int id, String nombre) {
+        this.idCategoria = id;
+        this.nombre = nombre;
+    }
+
+    public int getIdCategoria() { return idCategoria; }
+    public void setIdCategoria(int id) { this.idCategoria = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    @Override
+    public String toString() {
+        return "Categoria{id=" + idCategoria + ", nombre='" + nombre + "'}";
+    }
+}
